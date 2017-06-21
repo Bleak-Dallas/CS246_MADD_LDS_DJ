379bb520645c540cf59bb1dd36328e40f9dffd5b
Matt Burr <mdburr@outlook.com>
2017-06-21 18:06:54 -0500

    Updated CatalogActivity to retrieve a list from Firebase

--- app/src/main/java/edu/byui/maddldsdj/CatalogActivity.java
+++ app/src/main/java/edu/byui/maddldsdj/CatalogActivity.java
@@ -1,29 +1,71 @@
 package edu.byui.maddldsdj;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatalogActivity extends ListActivity {
 
+    FirebaseDatabase _firebase;
+    DatabaseReference _db;
+    private final static String CATALOG = "catalog";
+    private final static String TAG = "CatAct";
+    private Context _context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Catalog c = new Catalog();
-        c.load();
-        ArrayAdapter<Song> adapter = new CatalogAdapter(this, c.getSongs());
-        setListAdapter(adapter);
+        //Catalog c = new Catalog();
+        //c.load();
+        _context = this;
+        Log.d(TAG, "Connecting to FireBase...");
+
+        _firebase = FirebaseDatabase.getInstance();
+        _db = _firebase.getReference(CATALOG);
+
+        _db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Log.d(TAG, "Retrieving songs from Firebase...");
+                List<Song> songs = new ArrayList<>();
+
+                for (DataSnapshot song : dataSnapshot.getChildren())
+                    songs.add(song.getValue(Song.class));
+
+                ArrayAdapter<Song> adapter =
+                        new CatalogAdapter(_context, songs);
+
+                setListAdapter(adapter);
+                Log.d(TAG, "Songs retrieved.");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
