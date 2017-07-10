999bd30616b1f01c12be3b18d557b3cb2412d5f6
DamonSimpkinson <damonsimpkinson@gmail.com>
2017-07-10 05:08:17 -0400

    Added new navigation button for admin to take them to coming screen to display pending submissions for approval.  Made button to submit requests invisible for admin level users

--- app/src/main/java/edu/byui/maddldsdj/CatalogActivity.java
+++ app/src/main/java/edu/byui/maddldsdj/CatalogActivity.java
@@ -1,6 +1,8 @@
 package edu.byui.maddldsdj;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,18 +19,39 @@ import android.widget.Toast;
  */
 
 public class CatalogActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "CatalogActivity";
+    private static final String USERPREF = "UserPref";
     private Button buttonRequestApproval;
     private Button buttonViewPlaylist;
+    private Button buttonViewPending;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-        buttonRequestApproval = (Button) findViewById(R.id.button_Request_Approval);
+        buttonRequestApproval = (Button) findViewById(R.id.button_Request_Submission);
         buttonRequestApproval.setOnClickListener(this);
         buttonViewPlaylist = (Button) findViewById(R.id.button_View_Playlist);
         buttonViewPlaylist.setOnClickListener(this);
+        buttonViewPending = (Button) findViewById(R.id.button_View_Pending);
+        buttonViewPending.setOnClickListener(this);
+        setButtonVisibility();
+    }
+
+    private void setButtonVisibility() {
+        // get admin from Shared Preferences
+        SharedPreferences userPreferences = getSharedPreferences(USERPREF, Context.MODE_PRIVATE);
+        boolean useradmin = userPreferences.getBoolean("userAdmin", false);
+        // if admin show remove song button and hide vote buttons
+        if (useradmin) {
+            buttonViewPending.setVisibility(View.VISIBLE);
+            buttonRequestApproval.setVisibility(View.INVISIBLE);
+            // if NOT admin show vote buttons and hide remove song button
+        } else {
+            buttonViewPending.setVisibility(View.INVISIBLE);
+            buttonRequestApproval.setVisibility(View.VISIBLE);
+        }
     }
 
 
@@ -43,12 +66,19 @@ public class CatalogActivity extends AppCompatActivity implements View.OnClickLi
         }
 
         if (v == buttonRequestApproval){
-            Toast.makeText(CatalogActivity.this, "Request for song approval selected", Toast.LENGTH_SHORT).show();
+            Log.v(TAG, "Request Approval selected");
             // Create intent to display song details
             Intent dispSongRequest = new Intent(this, RequestSubmission.class);
-
             // Launch the intent
             startActivity(dispSongRequest);
+        }
+
+        if (v == buttonViewPending){
+            Log.v(TAG, "View Pending Approval List selected");
+            // create intent to display pending approval requests
+            Intent dispPendingApproval = new Intent(this, PendingApproval.class);
+            // launch the intent
+            startActivity(dispPendingApproval);
         }
     }
 }
